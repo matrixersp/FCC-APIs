b220583207181657e044d6141172d0b835149191
Mohammed Boudad <mboudad7@gmail.com>
2019-08-26 22:07:10 +0100

    Add Home page

--- Timestamp/server.js
+++ Timestamp/server.js
@@ -1,4 +1,11 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 app.get('/api/timestamp/:date_string?', function(req, res) {
   let str = req.params.date_string;
@@ -12,7 +19,8 @@ app.get('/api/timestamp/:date_string?', function(req, res) {
   res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log(listener.address(), process.env.PORT);
-  console.log('Your app is listening on port ' + listener.address().port);
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('The app is listening on port ' + port);
 });
